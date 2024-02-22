8de960a4a4302c1cbf88350ee9f3081c4a469d73
Kal2403 <kal2403199255@gmail.com>
2024-02-22 23:41:19 +0100

    Added event to change diferent type of format date

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
+
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch(dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+    default:
+      currentDateParagraph.textContent = formattedDate;
+  }
+});
